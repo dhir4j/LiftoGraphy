02a09991f95aa774e339cd0c9bea72e41450c014
dhir4j <kapsedhiraj15@gmail.com>
2022-06-26 17:01:45 +0530

    Enhanced home dashboard UI

--- app/src/main/java/com/example/gymbuddy/MainActivity.java
+++ app/src/main/java/com/example/gymbuddy/MainActivity.java
@@ -1,86 +1,54 @@
 package com.example.gymbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.annotation.SuppressLint;
+import androidx.cardview.widget.CardView;
 import android.content.Intent;
-import android.graphics.Color;
-import android.widget.*;
-import android.view.*;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    TextView txt1;
-    Button btn1,btn2,btn3,btn4;
-    @SuppressLint("SetTextI18n")
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    CardView cardWorkoutPlan, cardDietPlan, cardExercises, cardTools;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txt1 = (TextView) findViewById(R.id.txt1);
-        txt1.setText("G Y M  B U D D Y");
-        txt1.setTextSize(35);
+        cardWorkoutPlan = (CardView) findViewById(R.id.cardWorkoutPlan);
+        cardDietPlan = (CardView) findViewById(R.id.cardDietPlan);
+        cardExercises = (CardView) findViewById(R.id.cardExercises);
+        cardTools = (CardView) findViewById(R.id.cardTools);
 
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn1.setText("Weekly Routine");
-        btn1.setTextColor(Color.WHITE);
-        btn1.setTextSize(25);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity1();
-            }
-        });
+        cardWorkoutPlan.setOnClickListener(this);
+        cardDietPlan.setOnClickListener(this);
+        cardExercises.setOnClickListener(this);
+        cardTools.setOnClickListener(this);
 
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn2.setText("Exercises");
-        btn2.setTextColor(Color.WHITE);
-        btn2.setTextSize(25);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity2();
-            }
-        });
-
-        btn3 = (Button) findViewById(R.id.btn3);
-        btn3.setText("Diet");
-        btn3.setTextColor(Color.WHITE);
-        btn3.setTextSize(25);
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity3();
-            }
-        });
-
-        btn4 = (Button) findViewById(R.id.btn4);
-        btn4.setText("Tools");
-        btn4.setTextColor(Color.WHITE);
-        btn4.setTextSize(25);
-        btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity4();
-            }
-        });
 
     }
 
-    public void openActivity1(){
-        Intent intent1 = new Intent(this, Activity1.class);
-        startActivity(intent1);
-    }
-    public void openActivity2(){
-        Intent intent2 = new Intent(this, Activity2.class);
-        startActivity(intent2);
-    }
-    public void openActivity3(){
-        Intent intent3 = new Intent(this, Activity3.class);
-        startActivity(intent3);
-    }
-    public void openActivity4(){
-        Intent intent3 = new Intent(this, Activity4.class);
-        startActivity(intent3);
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()){
+            case R.id.cardWorkoutPlan:
+                i = new Intent(this, Activity1.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardDietPlan:
+                i = new Intent(this, Activity2.class);
+                startActivity(i);
+                break;
+            case R.id.cardExercises:
+                i = new Intent(this, Activity3.class);
+                startActivity(i);
+                break;
+            case R.id.cardTools:
+                i = new Intent(this, Activity4.class);
+                startActivity(i);
+                break;
+        }
     }
 }
